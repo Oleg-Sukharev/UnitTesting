fe0e70abb4d460bdd9c9726c1ada834f648ab0b8
Oleg <olegsukharev1991@gmail.com>
2024-04-16 00:08:18 +0200

    mocking

--- src/tests/mocking.test.js
+++ src/tests/mocking.test.js
@@ -1,4 +1,8 @@
 import { describe, vi, it, expect } from 'vitest';
+import { getPriceInCurrency } from '../mocking';
+import { getExchangeRate } from '../libs/currency';
+
+vi.mock('../libs/currency');
 
 describe('test suite', () => {
   it('test case', () => {
@@ -24,5 +28,24 @@ describe('test suite', () => {
     expect(greet).toBeCalled();
     expect(greet).toHaveBeenCalledOnce();
     expect(greet).toBeCalledWith('Jhon');
+  });
+
+  it('sendText', () => {
+    const sendText = vi.fn();
+    sendText.mockReturnValue('ok');
+    const result = sendText('message');
+
+    expect(sendText).toBeCalledWith('message');
+    expect(result).toBe('ok');
+  });
+});
+
+describe('getPriceInCurrency', () => {
+  it('should return price in target currency', () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+    const price = getPriceInCurrency(10, 'AUD');
+
+    expect(price).toBe(15);
   });
 });
